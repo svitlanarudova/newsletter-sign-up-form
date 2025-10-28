35769675b3680f4f8dcf107baa7f75b1b48a744e
svitlanarudova <rudovadev@gmail.com>
2025-10-28 13:12:52 +0400

    update 1

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const emailErrorMessageElement = document.querySelector("#email-error");
   const popup = document.querySelector("#success-popup");
   const closeBtn = document.querySelector(".popup__close");
+
   popup.inert = true;
+  let lastFocusedElement = null;
+
   function validateEmail(email) {
     if (!email) return "Email is required";
     const isValidEmail = /^\S+@\S+$/g;
@@ -13,28 +16,61 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     return "";
   }
+
   function handleSubmit(e) {
     e.preventDefault();
     const email = e.target.querySelector("#email").value;
     const emailErrorMessage = validateEmail(email);
+
     if (emailErrorMessage) {
       emailErrorMessageElement.innerText = emailErrorMessage;
       emailInput.classList.add("--error");
+      emailInput.focus();
+
     } else {
-      emailErrorMessageElement.innerText = "";
+      emailErrorMessageElement.textContent = '\u00A0';
       emailInput.classList.remove("--error");
+
+      lastFocusedElement = document.activeElement;
+
       const popupEmailElement = document.querySelector("#popup-email");
       popupEmailElement.textContent = email;
       setTimeout(() => {
         popup.classList.add("show");
         popup.inert = false;
+        closeBtn.focus();
         newsletterForm.reset();
       }, 300);
     }
   }
-  closeBtn.addEventListener("click", () => {
-    popup.classList.remove("show");
+
+  // closeBtn.addEventListener("click", () => {
+  //   popup.classList.remove("show");
+  //   popup.inert = true;
+  // });
+
+  function closePopup() {
+    popup.classList.remove('show');
     popup.inert = true;
+
+    if (lastFocusedElement) {
+      lastFocusedElement.focus();
+    }
+
+    setTimeout(() => {
+      popup.classList.add('hidden');
+    }, 200);
+  }
+
+  // close with btn
+  closeBtn.addEventListener('click', closePopup);
+
+  // close with escape
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && popup.classList.contains('show')) {
+      closePopup();
+    }
   });
+
   newsletterForm.addEventListener("submit", handleSubmit);
 });
